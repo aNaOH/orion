cacd3c0748194effd6dd0c51cafb2a380774df49
Abel <abehsosa2004@gmail.com>
2024-11-11 14:17:48 +0100

    added compose and modified SQL

--- orion.sql
+++ orion.sql
@@ -10,8 +10,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
-  KEY `fk_badge_id` (`badge_id`),
-  CONSTRAINT `fk_badge_id` FOREIGN KEY (`badge_id`) REFERENCES `badges` (`id`)
+  KEY `fk_badge_id` (`badge_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `developers` (
@@ -21,8 +20,7 @@ CREATE TABLE IF NOT EXISTS `developers` (
   `motd` varchar(255) DEFAULT NULL,
   `owner_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `owner_id` (`owner_id`),
-  CONSTRAINT `developers_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`)
+  KEY `owner_id` (`owner_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `game` (
@@ -37,8 +35,7 @@ CREATE TABLE IF NOT EXISTS `game` (
   `version` varchar(50) DEFAULT NULL,
   `developer_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_developer_id` (`developer_id`),
-  CONSTRAINT `fk_developer_id` FOREIGN KEY (`developer_id`) REFERENCES `developers` (`id`)
+  KEY `fk_developer_id` (`developer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `posts` (
@@ -53,9 +50,7 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `author_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `game_id` (`game_id`),
-  KEY `author_id` (`author_id`),
-  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`),
-  CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`)
+  KEY `author_id` (`author_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `achievements` (
@@ -67,8 +62,7 @@ CREATE TABLE IF NOT EXISTS `achievements` (
   `secret` tinyint(1) DEFAULT '0',
   `game_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `game_id` (`game_id`),
-  CONSTRAINT `achievements_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`)
+  KEY `game_id` (`game_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `badges` (
@@ -78,8 +72,7 @@ CREATE TABLE IF NOT EXISTS `badges` (
   `icon` varchar(255) NOT NULL,
   `game_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_game_id` (`game_id`),
-  CONSTRAINT `fk_game_id` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`)
+  KEY `fk_game_id` (`game_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `badge_unlocked` (
@@ -87,9 +80,7 @@ CREATE TABLE IF NOT EXISTS `badge_unlocked` (
   `user_id` int NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   KEY `badge_id` (`badge_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `badge_unlocked_ibfk_1` FOREIGN KEY (`badge_id`) REFERENCES `badges` (`id`),
-  CONSTRAINT `badge_unlocked_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `comments` (
@@ -100,9 +91,7 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `author_id` (`author_id`),
-  KEY `post_id` (`post_id`),
-  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
+  KEY `post_id` (`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `leaderboards` (
@@ -111,8 +100,7 @@ CREATE TABLE IF NOT EXISTS `leaderboards` (
   `type` int NOT NULL,
   `game_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `game_id` (`game_id`),
-  CONSTRAINT `leaderboards_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`)
+  KEY `game_id` (`game_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `entries` (
@@ -120,25 +108,20 @@ CREATE TABLE IF NOT EXISTS `entries` (
   `user_id` int NOT NULL,
   `value` int NOT NULL,
   KEY `leaderboard_id` (`leaderboard_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `entries_ibfk_1` FOREIGN KEY (`leaderboard_id`) REFERENCES `leaderboards` (`id`),
-  CONSTRAINT `entries_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `gallery_entries` (
   `post_id` int NOT NULL,
   `media` varchar(255) NOT NULL,
-  PRIMARY KEY (`post_id`),
-  CONSTRAINT `gallery_entries_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
+  PRIMARY KEY (`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `guides` (
   `post_id` int NOT NULL,
   `type_id` int NOT NULL,
   PRIMARY KEY (`post_id`),
-  KEY `type_id` (`type_id`),
-  CONSTRAINT `guides_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
-  CONSTRAINT `guides_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `guide_types` (`id`)
+  KEY `type_id` (`type_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `guide_types` (
@@ -154,9 +137,7 @@ CREATE TABLE IF NOT EXISTS `owns` (
   `base_price` float NOT NULL,
   `discount` float NOT NULL,
   KEY `user_id` (`user_id`),
-  KEY `game_id` (`game_id`),
-  CONSTRAINT `owns_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `owns_ibfk_2` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`)
+  KEY `game_id` (`game_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `unlocks` (
@@ -164,9 +145,7 @@ CREATE TABLE IF NOT EXISTS `unlocks` (
   `user_id` int NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   KEY `achievement_id` (`achievement_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `unlocks_ibfk_1` FOREIGN KEY (`achievement_id`) REFERENCES `achievements` (`id`),
-  CONSTRAINT `unlocks_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `votes` (
@@ -174,11 +153,64 @@ CREATE TABLE IF NOT EXISTS `votes` (
   `post_id` int NOT NULL,
   `is_downvote` tinyint(1) DEFAULT '0',
   KEY `user_id` (`user_id`),
-  KEY `post_id` (`post_id`),
-  CONSTRAINT `votes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `votes_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
+  KEY `post_id` (`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- Añadir las claves foráneas después de crear las tablas
+ALTER TABLE `users`
+  ADD CONSTRAINT `fk_badge_id` FOREIGN KEY (`badge_id`) REFERENCES `badges` (`id`);
+
+ALTER TABLE `developers`
+  ADD CONSTRAINT `developers_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `game`
+  ADD CONSTRAINT `fk_developer_id` FOREIGN KEY (`developer_id`) REFERENCES `developers` (`id`);
+
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`),
+  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `achievements`
+  ADD CONSTRAINT `achievements_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`);
+
+ALTER TABLE `badges`
+  ADD CONSTRAINT `fk_game_id` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`);
+
+ALTER TABLE `badge_unlocked`
+  ADD CONSTRAINT `badge_unlocked_ibfk_1` FOREIGN KEY (`badge_id`) REFERENCES `badges` (`id`),
+  ADD CONSTRAINT `badge_unlocked_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`);
+
+ALTER TABLE `leaderboards`
+  ADD CONSTRAINT `leaderboards_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`);
+
+ALTER TABLE `entries`
+  ADD CONSTRAINT `entries_ibfk_1` FOREIGN KEY (`leaderboard_id`) REFERENCES `leaderboards` (`id`),
+  ADD CONSTRAINT `entries_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `gallery_entries`
+  ADD CONSTRAINT `gallery_entries_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`);
+
+ALTER TABLE `guides`
+  ADD CONSTRAINT `guides_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
+  ADD CONSTRAINT `guides_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `guide_types` (`id`);
+
+ALTER TABLE `owns`
+  ADD CONSTRAINT `owns_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `owns_ibfk_2` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`);
+
+ALTER TABLE `unlocks`
+  ADD CONSTRAINT `unlocks_ibfk_1` FOREIGN KEY (`achievement_id`) REFERENCES `achievements` (`id`),
+  ADD CONSTRAINT `unlocks_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `votes`
+  ADD CONSTRAINT `votes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `votes_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`);
+
+-- Insert data
 INSERT INTO `users` (`id`, `email`, `username`, `password`, `role`, `profile_pic`, `motd`, `badge_id`, `created_at`) VALUES
 	(1, 'admin@togetheronorion.com', 'Orion', '$2y$10$jzNZPi0NpZoyB/VaJZkar.FVvw0YfUVT9CTgyuhJZt6lrxR.6wCZa', 1, "Orion's official account", NULL, NULL, '2024-10-30 15:24:12');
 
